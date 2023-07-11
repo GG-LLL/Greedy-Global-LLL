d8257d62fa8543cb7ea260407072b5c091fdaf57
jack-moyler <139053455+jack-moyler@users.noreply.github.com>
2023-07-11 15:32:43 +0100

    Update standalone_reduction.cpp

--- standalone_reduction.cpp
+++ standalone_reduction.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Program:     standalone_reduction
 
- * Copyright (C) 2023; Jack Moyler, Sanjay Bhattacherjee.
+ * Copyright (C) 2023; Sanjay Bhattacherjee, Jack Moyler.
 
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
