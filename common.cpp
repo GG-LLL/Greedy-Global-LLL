e54a33c90b637fd6ebbc5d3433a0950e64c413ce
jack-moyler <139053455+jack-moyler@users.noreply.github.com>
2023-07-11 15:30:33 +0100

    Update common.cpp

--- common.cpp
+++ common.cpp
@@ -2,6 +2,21 @@
  * Header File: common.cpp
  *              Contains definitions of all common functions required
  *              for lattice-based cryptography
+
+ * Copyright (C) 2023; Jack Moyler, Sanjay Bhattacherjee.
+
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *******************************************************************************/
 #include "common.h"
 
